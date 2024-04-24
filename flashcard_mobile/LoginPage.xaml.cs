912a227acee4106a5841f2ff20fa7b9fbb7e7c0b
Andrea Baulita <andybaulita@gmail.com>
2024-04-24 10:19:33 +0800

    initial login page

---
+++ flashcard_mobile/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace flashcard_mobile;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ flashcard_mobile/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace flashcard_mobile.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
